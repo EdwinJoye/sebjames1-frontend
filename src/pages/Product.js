033cda3c5e6bbee0ab2874999d8d2c75dc8afd50
Edwin <edwinjoye@gmail.com>
2023-07-04 19:42:17 +0200

    page product bien avancee

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,8 +1,11 @@
 import { useState } from "react";
+import Cadi from "../img/icons/cadi-white.png";
 import "../css/pages/product.css";
 
 const Product = () => {
   const [selectedImg, setSelectedImg] = useState(0);
+  const [quantity, setQuantity] = useState(1);
+
   const images = [
     "https://res.cloudinary.com/dm0sv9nfx/image/upload/v1687277100/sebjames/gravure_10_72354e0901.jpg",
     "https://res.cloudinary.com/dm0sv9nfx/image/upload/v1687277111/sebjames/gravure_11_c0480614a0.jpg",
@@ -19,6 +22,31 @@ const Product = () => {
       <div className="product__right">
         <h2>product</h2>
         <span>299$</span>
+        <p>
+          Le Lorem Ipsum est simplement du faux texte employé dans la
+          composition et la mise en page avant impression. Le Lorem Ipsum est le
+          faux texte standard de l'imprimerie depuis les années 1500, quand un
+          imprimeur anonyme assembla ensemble des morceaux de texte pour
+          réaliser un livre spécimen de polices de texte. Il n'a pas fait que
+          survivre cinq siècles, mais s'est aussi adapté à la bureautique
+          informatique, sans que son contenu n'en soit modifié. Il a été
+          popularisé dans les années 1960 grâce à la vente de feuilles Letraset
+          contenant des passages du Lorem Ipsum, et, plus récemment, par son
+          inclusion dans des applications de mise en page de texte, comme Aldus
+          PageMaker.
+        </p>
+        <div className="product__quantity">
+          <button
+            onClick={() => setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}
+          >
+            -
+          </button>
+          {quantity}
+          <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+        </div>
+        <div className="product__add-button">
+          <img src={Cadi} alt="" />
+        </div>
       </div>
     </div>
   );
