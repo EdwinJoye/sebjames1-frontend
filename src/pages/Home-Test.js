a40e9b980dc8f136ebed4f135f4d02f41c0c9fc7
Edwin <edwinjoye@gmail.com>
2023-07-26 18:18:31 +0200

    avance dans la hom

--- src/pages/Home-Test.js
+++ src/pages/Home-Test.js
@@ -7,7 +7,17 @@ import "../css/pages/home.css";
 
 const HomeTest = () => {
   const [scrollOpacity, setScrollOpacity] = useState(0);
+  const [isSticky, setIsSticky] = useState(true);
 
+  const handleScrollSticky = () => {
+    const scrollHeight = window.innerHeight * 1.2;
+
+    if (window.scrollY >= scrollHeight) {
+      setIsSticky(true);
+    } else {
+      setIsSticky(false);
+    }
+  };
   const handleScroll = () => {
     const scrollY = window.scrollY;
     const maxScroll = 1400;
@@ -16,24 +26,24 @@ const HomeTest = () => {
   };
 
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll, handleScrollSticky);
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("scroll", handleScroll, handleScrollSticky);
     };
   }, []);
 
   const getBackgroundColor = () => {
-    const maxOpacity = 1; // Maximum opacity value when the background becomes fully dark/black
+    const maxOpacity = 0.8;
     const opacityValue = maxOpacity * scrollOpacity;
-    return `rgba(0, 0, 0, ${opacityValue})`;
+    return `rgba(19, 19, 19, ${opacityValue})`;
   };
 
   return (
     <div
-      className="home__all-container"
+      className="home__all-container fadeIn"
       style={{ backgroundColor: getBackgroundColor() }}
     >
-      <div className="home__img-bg">
+      <div className={`home__img-bg ${isSticky ? "sticky" : "fixed"}`}>
         <img
           className="home__img"
           src="https://res.cloudinary.com/dm0sv9nfx/image/upload/v1690366008/sebjames/screen_1_e0bf17cd53.jpg"
@@ -41,7 +51,17 @@ const HomeTest = () => {
         />
       </div>
       <div className="home__content-container">
-        <div className="home__top-container"> HELLO SEB JAMES</div>
+        <div className="home__top-container">
+          <div className="home__title">
+            <div>
+              <span>Welcome in the</span>
+            </div>
+            <div className="home__title-secondPart">
+              <h1>WORLD</h1> <span>of</span>
+            </div>
+            <h1>SEB JAMES</h1>
+          </div>
+        </div>
         <div>
           <Propositions></Propositions>
           <FeaturedProducts type={"available"}></FeaturedProducts>
