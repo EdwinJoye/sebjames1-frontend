5127e461bc89690c84f31ffeda174b69c4beeb64
Edwin <edwinjoye@gmail.com>
2023-07-31 21:54:43 +0200

    test push

--- src/pages/Biography.js
+++ src/pages/Biography.js
@@ -1,5 +1,22 @@
+import "../css/pages/biography.css";
+
 const Biography = () => {
-  return <div className="container fadeIn">BIOGRAPHY</div>;
+  return (
+    <div className="biography fadeIn">
+      <section className="biography__section1">
+        <div className="biography__text">PHOTO</div>
+      </section>
+      <section className="biography__section2">
+        <div className="biography__text">CONTENU</div>
+      </section>
+      <section className="biography__section1">
+        <div className="biography__text">PHOTO</div>
+      </section>
+      <section className="biography__section2">
+        <div className="biography__text">CONTENU</div>
+      </section>
+    </div>
+  );
 };
 
 export default Biography;
