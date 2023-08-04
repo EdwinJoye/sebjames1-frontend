58d7213fa40878a8027c369246359577bbc7d764
Edwin <edwinjoye@gmail.com>
2023-08-04 12:09:42 +0200

    page interviews finies

--- src/pages/Interviews.js
+++ src/pages/Interviews.js
@@ -5,32 +5,39 @@ import "../css/pages/interviews.css";
 const Interviews = () => {
   const { data } = useFetch(`/interviews?populate=*`);
   return (
-    <div className="fadeIn">
+    <div className="interviews fadeIn">
       <div className="container">
-        {data?.map((item) => {
-          return (
-            <div className="interviews__all-container" key={item.id}>
-              <div className="interviews__infos-container">
-                <>
-                  <h2>{item.attributes.title}</h2>
-                  <div className="interviews__date">{item.attributes.date}</div>
-                </>
-                <span>{item.attributes.article}</span>
-                <a className="interviews__button" href={item.attributes.link}>
-                  Accéder au site
-                </a>
+        <h1>they interviewed Seb James</h1>
+        <div className="interviews__all-container1">
+          {data?.map((item) => {
+            return (
+              <div className="interviews__all-container" key={item.id}>
+                <div className="interviews__infos-container">
+                  <div>
+                    <>
+                      <h4>{item.attributes.title}</h4>
+                      <div className="interviews__date">
+                        {item.attributes.date}
+                      </div>
+                    </>
+                    <span>{item.attributes.article}</span>
+                  </div>
+                  <a className="interviews__button" href={item.attributes.link}>
+                    Accéder au site
+                  </a>
+                </div>
+                <div className="interviews__video-container">
+                  <video
+                    className="interviews__video"
+                    controls
+                    type="video/mp4"
+                    src={item.attributes.video.data[0].attributes.url}
+                  ></video>
+                </div>
               </div>
-              <div className="interviews__video-container">
-                <video
-                  className="interviews__video"
-                  controls
-                  type="video/mp4"
-                  src={item.attributes.video.data[0].attributes.url}
-                ></video>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
       <Footer />
     </div>
